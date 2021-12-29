7e03a8d627746b27056313c6841c5b6f16d73e61
PasinduManeka <manekaherat815@gmail.com>
2021-12-30 03:55:52 +0530

    Moved the upesert js part for external js file

---
+++ UserList/wwwroot/js/Upser.js
@@ -0,0 +1,38 @@
+﻿// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("btnUpsert").onclick = function () { BtnClick() };
+
+function BtnClick() {
+    //get the values from the input fields
+    var id = $("#id").val();
+    var name = $("#name").val();
+    var age = $("#age").val();
+    var city = $("#city").val();
+
+
+    var obj = {
+        ID: id,
+        Name: name,
+        Age: age,
+        City: city
+
+    }
+
+    //call the submitForm and pass the parameter
+    SubmitForm(obj)
+}
+
+function SubmitForm(obj) {
+    $.ajax({
+        url: "/Users/Upsert",
+        method: "POST",
+        data: obj,
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                setInterval('location.reload()', 500);
+            } else {
+                toastr.error(data.message);
+            }
+        }
+    })
+}
